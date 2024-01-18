399895eae4ca380cdb20d89ca7e938f64ea33247
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-18 16:34:53 +0300

    fix: remove the slash inside the paths from routes & change homepage component to new added component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,24 +2,27 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchpageComponent } from './searchpage/searchpage.component';
 import { WishlistpageComponent } from './wishlistpage/wishlistpage.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
         path: '',
         title: 'Movie App',
-        component: AppComponent,
+        component: HomeComponent,
     },
     {
-        path: '/search',
+        path: 'search',
         title: 'Search | Movie App',
         component: SearchpageComponent,
     },
     {
-        path: '/wishlist',
+        path: 'wishlist',
         title: 'Wishlist | Movie App',
         component: WishlistpageComponent,
     },
     {
-        path: '/movie/:id',
+        path: 'movie/:id',
+        component: MovieDetailComponent,
     }
 ];
