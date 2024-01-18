d26c8ced8ffd6f2c6b3128f330152afb793c0859
Mehmet Emin Özkürkçüler <boraa.durna@gmail.com>
2024-01-18 16:37:07 +0300

    feat: add movie lists & movie data fetching functions to movie service

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -8,6 +8,10 @@ import { environment } from '../../environments/environment';
 export class MovieService {
   private URL = 'https://api.themoviedb.org/';
   private http = inject(HttpClient);
+  private popularMoviesUrl = `${this.URL}/3/movie/popular?language=en-US&page=1`
+  private topRatedMoviesUrl = `${this.URL}/3/movie/top_rated?language=en-US&page=1`
+  private nowPlayingMoviesUrl = `${this.URL}/3/movie/now_playing?language=en-US&page=1`
+  private upcomingMoviesUrl = `${this.URL}/3/movie/upcoming?language=en-US&page=1`
 
   private options = {
     method: 'GET',
@@ -19,10 +23,30 @@ export class MovieService {
 
   getPopularMovies(): Observable<Movie[]> {
     return this.http
-      .get<{ results: Movie[] }>(
-        `${this.URL}/3/movie/popular?language=en-US&page=1`,
-        this.options
-      )
-      .pipe(map((response) => response.results));
+      .get<{ results: Movie[] }>(this.popularMoviesUrl, this.options).pipe(map((response) => response.results));
+  }
+
+  getTopRatedMovies(): Observable<Movie[]> {
+    return this.http.get<{ results: Movie[] }>(this.topRatedMoviesUrl, this.options).pipe(map((response) => response.results));
+  }
+
+  getNowPlayingMovies(): Observable<Movie[]> {
+    return this.http.get<{ results: Movie[] }>(this.nowPlayingMoviesUrl, this.options).pipe(map((response) => response.results));
+  }
+
+  getUpcomingMovies(): Observable<Movie[]> {
+    return this.http.get<{ results: Movie[] }>(this.upcomingMoviesUrl, this.options).pipe(map((response) => response.results));
+  }
+
+  getMovieDetail(id: number): Observable<Array<Object>> {
+    return this.http.get<Array<Object>>(this.URL + `3/movie/${id}?language=en-US`, this.options).pipe((response) => response);
+  }
+
+  getMovieReviews(id: number): Observable<Array<Object>> {
+    return this.http.get<Array<Object>>(this.URL + `3/movie/${id}/reviews?language=en-US&page=1`, this.options).pipe((response) => response);
+  }
+
+  getMovieCredits(id: number): Observable<Array<Object>> {
+    return this.http.get<Array<Object>>(this.URL + `3/movie/${id}/credits?language=en-US`, this.options).pipe((response) => response);
   }
 }
