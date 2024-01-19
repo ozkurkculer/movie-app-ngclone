f33bb7c1d2ed1f4513a4b1bcad3fdcd1f65ed727
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-19 21:08:17 +0300

    feat: add canactivate parameter to route homepage if id missing or wrong

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
-import { Routes } from '@angular/router';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SearchpageComponent } from './searchpage/searchpage.component';
 import { WishlistpageComponent } from './wishlistpage/wishlistpage.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { HomeComponent } from './home/home.component';
+import { inject } from '@angular/core';
 
 export const routes: Routes = [
     {
@@ -24,5 +25,14 @@ export const routes: Routes = [
     {
         path: 'movie/:id',
         component: MovieDetailComponent,
+        canActivate: [
+            (route: ActivatedRouteSnapshot) => {
+                const id = route.paramMap.get('id')
+                if (!id || isNaN(Number(id))) {
+                    return inject(Router).createUrlTree([''])
+                } 
+                return true;
+            }
+        ]
     }
 ];
