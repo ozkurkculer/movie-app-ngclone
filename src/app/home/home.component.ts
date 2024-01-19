fd057effc69442fc08773bbedb8079a3d514e345
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-19 21:12:44 +0300

    feat: add movie poster images, categories, slider poster images and searchbar to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { TopratedMovieListComponent } from '../toprated-movie-list/toprated-movie-list.component';
 import { MovieListCategoriesComponent } from '../movie-list-categories/movie-list-categories.component';
+import { SearchbarComponent } from '../searchbar/searchbar.component';
 
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterLink, TopratedMovieListComponent, MovieListCategoriesComponent],
+  imports: [RouterLink, TopratedMovieListComponent, MovieListCategoriesComponent, SearchbarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
