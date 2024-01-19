debd4595b06a304dbc16966422afe411cdcc6fdd
Mehmet Emin Özkürkçüler <mehmetemin.ozkurkculer@gmail.com>
2024-01-19 21:06:20 +0300

    feat: remove unused imports and comment codes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterModule } from '@angular/router';
-import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -11,10 +10,4 @@ import { Title } from '@angular/platform-browser';
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-
-  // id = inject(ActivatedRoute).snapshot.paramMap.get('id');
-  // private titleService = inject(Title);
-  // setTitle() {
-  //   this.titleService.setTitle();
-  // }
 }
